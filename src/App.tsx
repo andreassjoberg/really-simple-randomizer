8db5d8f07fb18aa85c994497ed2a0b045765d63c
Victor Alvarenga <info@victoralvarenga.com>
2020-10-24 21:22:46 -0300

    Formating: App 4 spaces to 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,62 +9,63 @@ import InputForm from "./InputForm";
 import OutputBox from "./OutputBox";
 import Footer from "./Footer";
 
-
 const getRandomNumber = (max: number) => Math.floor(Math.random() * max);
 
 const App = () => {
-    const [names, setNames] = useState<string[]>([]);
-    const [winners, setWinners] = useState<number>(0);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [names, setNames] = useState<string[]>([]);
+  const [winners, setWinners] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const alert = useAlert();
+  const alert = useAlert();
 
-    const randomize = useCallback((names: string[]) => {
-        return new Promise<string[]>(resolve => {
-            setTimeout(() => {
-                let unrandomized = [...names];
-                let randomized = [];
+  const randomize = useCallback((names: string[]) => {
+    return new Promise<string[]>((resolve) => {
+      setTimeout(() => {
+        let unrandomized = [...names];
+        let randomized = [];
 
-                while (unrandomized.length > 0) {
-                    let nextIndex = getRandomNumber(unrandomized.length);
-                    let pickedItem = unrandomized[nextIndex];
-                    randomized.push(pickedItem);
-                    unrandomized = unrandomized.filter((_item, index) => index !== nextIndex);
-                }
+        while (unrandomized.length > 0) {
+          let nextIndex = getRandomNumber(unrandomized.length);
+          let pickedItem = unrandomized[nextIndex];
+          randomized.push(pickedItem);
+          unrandomized = unrandomized.filter(
+            (_item, index) => index !== nextIndex
+          );
+        }
 
-                resolve(randomized);
-            }, 0);
-        });
-    },[]);
+        resolve(randomized);
+      }, 0);
+    });
+  }, []);
 
-    const namesPosted = useCallback((names: string[], winners: number) => {
-        setIsLoading(true);
-        setWinners(winners > 0 ? winners : 3);
+  const namesPosted = useCallback(
+    (names: string[], winners: number) => {
+      setIsLoading(true);
+      setWinners(winners > 0 ? winners : 3);
 
-        randomize(names).then(randomized => {
-            setNames(randomized);
-            setIsLoading(false);
+      randomize(names).then((randomized) => {
+        setNames(randomized);
+        setIsLoading(false);
 
-            alert.success("Randomization complete!");
-        });
-    }, [alert, randomize]);
+        alert.success("Randomization complete!");
+      });
+    },
+    [alert, randomize]
+  );
 
-    return (
-        <div className="container">
-        <Header />
+  return (
+    <div className="container">
+      <Header />
 
-        <Loader isLoading={isLoading} />
+      <Loader isLoading={isLoading} />
 
-        <InputForm
-            names={names}
-            postInput={namesPosted}
-        />
+      <InputForm names={names} postInput={namesPosted} />
 
-        <OutputBox names={names} winners={winners} />
+      <OutputBox names={names} winners={winners} />
 
-        <Footer />
+      <Footer />
     </div>
-    );
+  );
 };
 
 export default App;
